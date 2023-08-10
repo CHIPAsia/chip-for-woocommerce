492718efe8402d703cbc39fcdf53dd4fdb33b3c6
Azuddin <ahmad@azuddin.com>
2023-08-10 19:50:53 +0800

    include payment method with payload

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '0a7b95f88eb2267a58da980b4ed71706');
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'e397564b5888e06f7d17b2f3e7dbdef5');
--- resources/js/frontend/blocks.js
+++ resources/js/frontend/blocks.js
@@ -1,217 +1,213 @@
-import { registerPaymentMethod } from '@woocommerce/blocks-registry';
-import { __ } from '@wordpress/i18n';
-import { decodeEntities } from '@wordpress/html-entities';
-import { getSetting } from '@woocommerce/settings';
-import { TreeSelect } from '@wordpress/components';
-import { useState } from '@wordpress/element';
+import { registerPaymentMethod } from "@woocommerce/blocks-registry";
+import { __ } from "@wordpress/i18n";
+import { decodeEntities } from "@wordpress/html-entities";
+import { getSetting } from "@woocommerce/settings";
+import { TreeSelect } from "@wordpress/components";
+import { useState, useEffect } from "@wordpress/element";
 
-const settings = getSetting( GATEWAY.id + '_data', {} );
+const settings = getSetting(GATEWAY.id + "_data", {});
 
-const defaultLabel = __(
-	'CHIP',
-	'chip-for-woocommerce'
-);
+const defaultLabel = __("CHIP", "chip-for-woocommerce");
 
 /**
  * console.log(wc.wcSettings);
  * wc.wcSettings.allSettings;
  */
 
-const label = decodeEntities( settings.title ) || defaultLabel;
+const label = decodeEntities(settings.title) || defaultLabel;
 
 const Content = () => {
-  return decodeEntities( settings.description || '' );
+  return decodeEntities(settings.description || "");
 };
 
-const Label = ( props ) => {
+const Label = (props) => {
   const { PaymentMethodLabel } = props.components;
-  return <PaymentMethodLabel text={ label } />;
+  return <PaymentMethodLabel text={label} />;
 };
 
-// const setPageAA = ( value ) => {
-//   console.log(value);
-// }
+const FpxBankList = (props) => {
+  const [bankId, setBankId] = useState(undefined);
+  const { eventRegistration, emitResponse } = props;
+  const { onPaymentProcessing } = eventRegistration;
 
-let setPageAA = '';
+  const onSubmit = () => {
+    if (undefined === bankId) {
+      return {
+        type: emitResponse.responseTypes.ERROR,
+        message: __(
+          "<strong>Internet Banking</strong> is a required field.",
+          "chip-for-woocommerce"
+        ),
+      };
+    }
 
-const FpxBankList = () => {
-    const [ page, setPage ] = useState( 'p21' );
+    return {
+      type: emitResponse.responseTypes.SUCCESS,
+      meta: {
+        paymentMethodData: {
+          chip_fpx_bank: bankId,
+        },
+      },
+    };
+  };
 
-    return (
-        <TreeSelect
-            label="Internet Banking"
-            noOptionLabel="Select Bank"
-            onChange={ 
-              ( newPage ) => { 
-                setPageAA = newPage;
-                setPage( newPage );
-              }
-            }
-            selectedId={ page }
-            tree={ [
-                {
-                    name: 'Affin Bank',
-                    id: 'ABB0233',
-                },
-                {
-                    name: 'Alliance Bank (Personal)',
-                    id: 'ABMB0212',
-                },
-                {   name: 'AGRONet',
-                    id: 'AGRO01', 
-                },
-                {   name: 'AmBank',
-                    id: 'AMBB0209', 
-                },
-                {   name: 'Bank Islam',
-                    id: 'BIMB0340', 
-                },
-                {   name: 'Bank Muamalat',
-                    id: 'BMMB0341', 
-                },
-                {   name: 'Bank Rakyat',
-                    id: 'BKRM0602', 
-                },
-                {   name: 'Bank Of China',
-                    id: 'BOCM01', 
-                },
-                {   name: 'BSN',
-                    id: 'BSN0601', 
-                },
-                {   name: 'CIMB Bank',
-                    id: 'BCBB0235', 
-                },
-                {   name: 'Hong Leong Bank',
-                    id: 'HLB0224', 
-                },
-                {   name: 'HSBC Bank',
-                    id: 'HSBC0223', 
-                },
-                {   name: 'KFH',
-                    id: 'KFH0346', 
-                },
-                {   name: 'Maybank2E',
-                    id: 'MBB0228', 
-                },
-                {   name: 'Maybank2u',
-                    id: 'MB2U0227', 
-                },
-                {   name: 'OCBC Bank',
-                    id: 'OCBC0229', 
-                },
-                {   name: 'Public Bank',
-                    id: 'PBB0233', 
-                },
-                {   name: 'RHB Bank',
-                    id: 'RHB0218', 
-                },
-                {   name: 'Standard Chartered',
-                    id: 'SCB0216', 
-                },
-                {   name: 'UOB Bank',
-                    id: 'UOB0226', 
-                }
-            ] }
-        />
-    );
-}
-
-const Fpxb2b1BankList = () => {
-  const [ page, setPage ] = useState( 'p21' );
+  useEffect(() => {
+    const unsubscribeProcessing = onPaymentProcessing(onSubmit);
+    return () => {
+      unsubscribeProcessing();
+    };
+  }, [onPaymentProcessing, bankId]);
 
   return (
-      <TreeSelect
-          label="Internet Banking"
-          noOptionLabel="Select Bank"
-          onChange={ ( newPage ) => setPage( newPage ) }
-          selectedId={ page }
-          tree={ [
-              {   name: 'AFFINMAX',
-                  id: 'ABB0235',
-              },
-              {   name: 'Alliance Bank (Business)',
-                  id: 'ABMB0213',
-              },
-              {   name: 'AGRONetBIZ',
-                  id: 'AGRO02',
-              },
-              {   name: 'AmBank',
-                  id: 'AMBB0208',
-              },
-              {   name: 'Bank Islam',
-                  id: 'BIMB0340',
-              },
-              {   name: 'Bank Muamalat',
-                  id: 'BMMB0342',
-              },
-              {   name: 'BNP Paribas',
-                  id: 'BNP003',
-              },
-              {   name: 'CIMB Bank',
-                  id: 'BCBB0235',
-              },
-              {   name: 'Citibank Corporate Banking',
-                  id: 'CIT0218',
-              },
-              {   name: 'Deutsche Bank',
-                  id: 'DBB0199',
-              },
-              {   name: 'Hong Leong Bank',
-                  id: 'HLB0224',
-              },
-              {   name: 'HSBC Bank',
-                  id: 'HSBC0223',
-              },
-              {   name: 'Bank Rakyat',
-                  id: 'BKRM0602',
-              },
-              {   name: 'KFH',
-                  id: 'KFH0346',
-              },
-              {   name: 'Maybank2E',
-                  id: 'MBB0228',
-              },
-              {   name: 'OCBC Bank',
-                  id: 'OCBC0229',
-              },
-              {   name: 'Public Bank',
-                  id: 'PBB0233',
-              },
-              {   name: 'Public Bank PB enterprise',
-                  id: 'PBB0234',
-              },
-              {   name: 'RHB Bank',
-                  id: 'RHB0218',
-              },
-              {   name: 'Standard Chartered',
-                  id: 'SCB0215',
-              },
-              {   name: 'UOB Regional',
-                  id: 'UOB0228',
-              },
-          ] }
-      />
+    <TreeSelect
+      label={__("Internet Banking", "chip-for-woocommerce")}
+      noOptionLabel={__("Choose an option", "chip-for-woocommerce")}
+      onChange={(selected_bank_id) => {
+        setBankId(selected_bank_id);
+      }}
+      selectedId={bankId}
+      tree={[
+        {
+          name: __("Affin Bank", "chip-for-woocommerce"),
+          id: "ABB0233",
+        },
+        {
+          name: __("Alliance Bank (Personal)", "chip-for-woocommerce"),
+          id: "ABMB0212",
+        },
+        { name: __("AGRONet", "chip-for-woocommerce"), id: "AGRO01" },
+        { name: __("AmBank", "chip-for-woocommerce"), id: "AMBB0209" },
+        { name: __("Bank Islam", "chip-for-woocommerce"), id: "BIMB0340" },
+        { name: __("Bank Muamalat", "chip-for-woocommerce"), id: "BMMB0341" },
+        { name: __("Bank Rakyat", "chip-for-woocommerce"), id: "BKRM0602" },
+        { name: __("Bank Of China", "chip-for-woocommerce"), id: "BOCM01" },
+        { name: __("BSN", "chip-for-woocommerce"), id: "BSN0601" },
+        { name: __("CIMB Bank", "chip-for-woocommerce"), id: "BCBB0235" },
+        {
+          name: __("Hong Leong Bank", "chip-for-woocommerce"),
+          id: "HLB0224",
+        },
+        { name: __("HSBC Bank", "chip-for-woocommerce"), id: "HSBC0223" },
+        { name: __("KFH", "chip-for-woocommerce"), id: "KFH0346" },
+        { name: __("Maybank2E", "chip-for-woocommerce"), id: "MBB0228" },
+        { name: __("Maybank2u", "chip-for-woocommerce"), id: "MB2U0227" },
+        { name: __("OCBC Bank", "chip-for-woocommerce"), id: "OCBC0229" },
+        { name: __("Public Bank", "chip-for-woocommerce"), id: "PBB0233" },
+        { name: __("RHB Bank", "chip-for-woocommerce"), id: "RHB0218" },
+        {
+          name: __("Standard Chartered", "chip-for-woocommerce"),
+          id: "SCB0216",
+        },
+        { name: __("UOB Bank", "chip-for-woocommerce"), id: "UOB0226" },
+      ]}
+    />
   );
-}
+};
 
-// fragment --> <> --> untuk wrap bila return mesti 1 component sahaja.
-const content_display = <><Content />
-{ settings.js_display == 'fpx' ? <FpxBankList /> : null }
-{ settings.js_display == 'fpx_b2b1' ? <Fpxb2b1BankList /> : null}
-</>;
+const Fpxb2b1BankList = (props) => {
+  const [bankIdB2b, setBankIdB2b] = useState(undefined);
+  const { eventRegistration, emitResponse } = props;
+  const { onPaymentProcessing } = eventRegistration;
+
+  const onSubmit = () => {
+    if (undefined === bankIdB2b) {
+      return {
+        type: emitResponse.responseTypes.ERROR,
+        message: __(
+          "<strong>Corporate Internet Banking</strong> is a required field.",
+          "chip-for-woocommerce"
+        ),
+      };
+    }
+
+    return {
+      type: emitResponse.responseTypes.SUCCESS,
+      meta: {
+        paymentMethodData: {
+          chip_fpx_b2b1_bank: bankIdB2b,
+        },
+      },
+    };
+  };
+
+  useEffect(() => {
+    const unsubscribeProcessing = onPaymentProcessing(onSubmit);
+    return () => {
+      unsubscribeProcessing();
+    };
+  }, [onPaymentProcessing, bankIdB2b]);
+
+  return (
+    <TreeSelect
+      label={__("Internet Banking", "chip-for-woocommerce")}
+      noOptionLabel={__("Choose an option", "chip-for-woocommerce")}
+      onChange={(selected_bank_id) => {
+        setBankIdB2b(selected_bank_id);
+      }}
+      selectedId={bankIdB2b}
+      tree={[
+        { name: __("AFFINMAX", "chip-for-woocommerce"), id: "ABB0235" },
+        {
+          name: __("Alliance Bank (Business)", "chip-for-woocommerce"),
+          id: "ABMB0213",
+        },
+        { name: __("AGRONetBIZ", "chip-for-woocommerce"), id: "AGRO02" },
+        { name: __("AmBank", "chip-for-woocommerce"), id: "AMBB0208" },
+        { name: __("Bank Islam", "chip-for-woocommerce"), id: "BIMB0340" },
+        { name: __("Bank Muamalat", "chip-for-woocommerce"), id: "BMMB0342" },
+        { name: __("BNP Paribas", "chip-for-woocommerce"), id: "BNP003" },
+        { name: __("CIMB Bank", "chip-for-woocommerce"), id: "BCBB0235" },
+        {
+          name: __("Citibank Corporate Banking", "chip-for-woocommerce"),
+          id: "CIT0218",
+        },
+        { name: __("Deutsche Bank", "chip-for-woocommerce"), id: "DBB0199" },
+        { name: __("Hong Leong Bank", "chip-for-woocommerce"), id: "HLB0224" },
+        { name: __("HSBC Bank", "chip-for-woocommerce"), id: "HSBC0223" },
+        { name: __("Bank Rakyat", "chip-for-woocommerce"), id: "BKRM0602" },
+        { name: __("KFH", "chip-for-woocommerce"), id: "KFH0346" },
+        { name: __("Maybank2E", "chip-for-woocommerce"), id: "MBB0228" },
+        { name: __("OCBC Bank", "chip-for-woocommerce"), id: "OCBC0229" },
+        { name: __("Public Bank", "chip-for-woocommerce"), id: "PBB0233" },
+        {
+          name: __("Public Bank PB enterprise", "chip-for-woocommerce"),
+          id: "PBB0234",
+        },
+        { name: __("RHB Bank", "chip-for-woocommerce"), id: "RHB0218" },
+        {
+          name: __("Standard Chartered", "chip-for-woocommerce"),
+          id: "SCB0215",
+        },
+        { name: __("UOB Regional", "chip-for-woocommerce"), id: "UOB0228" },
+      ]}
+    />
+  );
+};
+
+const ContentContainer = (props) => {
+  return (
+    <>
+      <Content />
+      {settings.js_display == "fpx" ? <FpxBankList {...props} /> : null}
+      {settings.js_display == "fpx_b2b1" ? (
+        <Fpxb2b1BankList {...props} />
+      ) : null}
+    </>
+  );
+};
 
 const wc_gateway_chip = {
   name: settings.method_name,
   label: <Label />,
-  content: content_display,
-  edit: content_display,
+  content: <ContentContainer />,
+  edit: <ContentContainer />,
   canMakePayment: () => true,
   ariaLabel: label,
   supports: {
     showSavedCards: settings.saved_option,
-		showSaveOption: settings.save_option,
+    showSaveOption: settings.save_option,
     features: settings.supports,
   },
 };
 
-registerPaymentMethod( wc_gateway_chip );
+registerPaymentMethod(wc_gateway_chip);
