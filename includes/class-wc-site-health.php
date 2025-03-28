537a562544478e4dd986aa998dc0a5f05d3671d6
Amirul Azreen <amirulazreen1994@gmail.com>
2025-03-28 12:17:33 +0800

    fix: follow wordpress coding style

--- includes/class-wc-site-health.php
+++ includes/class-wc-site-health.php
@@ -1,78 +1,78 @@
 <?php
 
-if (! defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-class CHIP_Woocommerce_Site_Health
-{
+class CHIP_Woocommerce_Site_Health {
 
-    public function __construct()
-    {
-        add_filter('site_status_tests', [$this, 'CHIP_plugin_register_site_health_tests']);
-    }
 
-    public function CHIP_plugin_register_site_health_tests($tests)
-    {
-        $tests['direct']['CHIP_plugin_api_status'] = [
-            'test'  => [$this, 'CHIP_plugin_check_api_health'],
-            'label' => 'CHIP Plugin Connection Status',
-        ];
-        return $tests;
-    }
+	public function __construct() {
+		add_filter( 'site_status_tests', array( $this, 'CHIP_plugin_register_site_health_tests' ) );
+	}
 
-    public function CHIP_plugin_check_api_health()
-    {
-        $purchase_API = WC_CHIP_ROOT_URL . '/v1/purchases/';
-        $response     = wp_remote_get($purchase_API);
+	public function CHIP_plugin_register_site_health_tests( $tests ) {
+		$tests['direct']['CHIP_plugin_api_status'] = array(
+			'test'  => array( $this, 'CHIP_plugin_check_api_health' ),
+			'label' => 'CHIP Plugin Connection Status',
+		);
+		return $tests;
+	}
 
-        $status_code = wp_remote_retrieve_response_code($response);
+	public function CHIP_plugin_check_api_health() {
+		$purchase_API = WC_CHIP_ROOT_URL . '/v1/purchases/';
+		$response     = wp_remote_get( $purchase_API );
+    
 
-        switch ($status_code) {
-            case 401:
-                return $this->response_success();
-            default:
-                return $this->response_fail();
-        }
-    }
 
-    public function response_success()
-    {
-        return [
-            'label'       => 'CHIP API is working',
-            'status'      => 'good',
-            'badge'       => [
-                'label' => 'CHIP Plugin',
-                'color' => 'green',
-            ],
-            'description' => sprintf(
-                '<p>%s</p>',
-                'CHIP API is responding correctly.'
-            ),
-            'actions'     => [],
-            'test'        => 'CHIP_plugin_api_status',
-        ];
-    }
+		$status_code = wp_remote_retrieve_response_code( $response );
 
-    public function response_fail()
-    {
-        return [
-            'label'       => 'API Issue Detected',
-            'status'      => 'critical',
-            'badge'       => [
-                'label' => 'CHIP Plugin',
-                'color' => 'red',
-            ],
-            'description' => sprintf(
-                '<p>%s</p>',
-                'Unable to connect to CHIP API.'
-            ),
-            'actions'     => [],
-            'test'        => 'CHIP_plugin_api_status',
-        ];
-    }
+		switch ( $status_code ) {
+			case 401:
+				return $this->response_success();
+			default:
+				return $this->response_fail();
+		}
+	}
+
+	public function response_success() {
+		return array(
+			'label'       => 'CHIP API is working',
+			'status'      => 'good',
+			'badge'       => array(
+				'label' => 'CHIP Plugin',
+				'color' => 'green',
+			),
+			'description' => sprintf(
+				'<p>%s</p>',
+				'CHIP API is responding correctly.'
+			),
+			'actions'     => array(),
+			'test'        => 'CHIP_plugin_api_status',
+		);
+	}
+
+	public function response_fail() {
+		return array(
+			'label'       => 'API Issue Detected',
+			'status'      => 'critical',
+			'badge'       => array(
+				'label' => 'CHIP Plugin',
+				'color' => 'red',
+			),
+			'description' => sprintf(
+				'<p>%s</p>',
+				'Unable to connect to CHIP API.'
+			),
+			'actions'     => array(),
+			'test'        => 'CHIP_plugin_api_status',
+		);
+	}
 }
 
-add_action('init', function () {
-    new CHIP_Woocommerce_Site_Health();
-});
+add_action(
+	'init',
+	function () {
+		new CHIP_Woocommerce_Site_Health();
+	}
+);
