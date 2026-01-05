ee9f640bf00e915076778c51f3887be796513cec
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2026-01-05 11:06:34 +0800

    Add price divider option

--- includes/class-chip-woocommerce-gateway-5.php
+++ includes/class-chip-woocommerce-gateway-5.php
@@ -22,6 +22,20 @@ class Chip_Woocommerce_Gateway_5 extends Chip_Woocommerce_Gateway {
 	const PREFERRED_TYPE = 'Atome';
 
 	/**
+	 * Price divider product detail setting.
+	 *
+	 * @var string
+	 */
+	protected $price_divider_product_detail;
+
+	/**
+	 * Price divider product list setting.
+	 *
+	 * @var string
+	 */
+	protected $price_divider_product_list;
+
+	/**
 	 * Initialize gateway ID.
 	 *
 	 * @return void
@@ -36,6 +50,13 @@ class Chip_Woocommerce_Gateway_5 extends Chip_Woocommerce_Gateway {
 	public function __construct() {
 		parent::__construct();
 		$this->payment_method_whitelist = array( 'razer_atome' );
+
+		// Load price divider settings.
+		$this->price_divider_product_detail = $this->get_option( 'price_divider_product_detail' );
+		$this->price_divider_product_list   = $this->get_option( 'price_divider_product_list' );
+
+		// Enqueue price divider script on frontend.
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_price_divider_script' ) );
 	}
 
 	/**
@@ -95,5 +116,101 @@ class Chip_Woocommerce_Gateway_5 extends Chip_Woocommerce_Gateway {
 		unset( $this->form_fields['payment_method_whitelist'] );
 		unset( $this->form_fields['payment_action'] );
 		unset( $this->form_fields['bypass_chip'] );
+
+		// Add Price Divider settings.
+		$this->form_fields['price_divider_settings'] = array(
+			'title' => __( 'Price Divider Settings', 'chip-for-woocommerce' ),
+			'type'  => 'title',
+		);
+
+		$this->form_fields['price_divider_product_detail'] = array(
+			'title'       => __( 'Price Divider (Product Detail)', 'chip-for-woocommerce' ),
+			'type'        => 'checkbox',
+			'label'       => __( 'Price Divider (Product Detail)', 'chip-for-woocommerce' ),
+			'description' => __( 'Show Atome price divider on product detail pages.', 'chip-for-woocommerce' ),
+			'default'     => 'no',
+		);
+
+		$this->form_fields['price_divider_product_list'] = array(
+			'title'       => __( 'Price Divider (Product List)', 'chip-for-woocommerce' ),
+			'type'        => 'checkbox',
+			'label'       => __( 'Price Divider (Product List)', 'chip-for-woocommerce' ),
+			'description' => __( 'Show Atome price divider on product list pages.', 'chip-for-woocommerce' ),
+			'default'     => 'no',
+		);
+	}
+
+	/**
+	 * Enqueue Atome price divider script.
+	 *
+	 * @return void
+	 */
+	public function enqueue_price_divider_script() {
+		// Check if gateway is enabled.
+		if ( 'yes' !== $this->enabled ) {
+			return;
+		}
+
+		$product_detail = 'yes' === $this->price_divider_product_detail;
+		$product_list   = 'yes' === $this->price_divider_product_list;
+
+		// If neither option is checked, don't enqueue anything.
+		if ( ! $product_detail && ! $product_list ) {
+			return;
+		}
+
+		// Determine apply_on value.
+		if ( $product_detail && $product_list ) {
+			$apply_on = 'all';
+		} elseif ( $product_detail ) {
+			$apply_on = 'product';
+		} else {
+			$apply_on = 'list';
+		}
+
+		$country     = 'my';
+		$language    = 'en';
+		$environment = 'production';
+
+		$options = array(
+			'is_atome_enabled'         => 'yes',
+			'country'                  => $country,
+			'language'                 => $language,
+			'api_environment'          => $environment,
+			'price_divider'            => 'yes',
+			'price_divider_applied_on' => $apply_on,
+			'sku_permission'           => 'yes',
+			'max_spend'                => '',
+			'min_spend'                => '',
+			'cancel_timeout'           => '720',
+			'debug_mode'               => 'no',
+			'version'                  => '6.7.0',
+			'platform'                 => 'WOOCOMMERCE',
+			'enable_send_errors'       => 'no',
+			'error_report_url'         => get_site_url() . '/?wc-api=atome_error_report',
+		);
+
+		$static_file_domain = 'https://atome-paylater-fe.s3-accelerate.amazonaws.com/merchant-plugins/production/static';
+
+		// Register and enqueue the Atome price divider script.
+		wp_register_script(
+			'atome-price-divider',
+			$static_file_domain . '/price_divider/main.js',
+			array(),
+			'6.7.0',
+			array(
+				'strategy'  => 'defer',
+				'in_footer' => false,
+			)
+		);
+
+		// Add inline script with options before the main script.
+		wp_add_inline_script(
+			'atome-price-divider',
+			'window.atomePaymentPluginPriceDividerOptions = ' . wp_json_encode( $options ) . ';',
+			'before'
+		);
+
+		wp_enqueue_script( 'atome-price-divider' );
 	}
 }
